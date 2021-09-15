143d9bdbecfbfc65bca08752fd3a9e1357261282
Calapzu <yamsoncalapzu@gmail.com>
2021-09-15 00:11:23 -0500

    Se termina de colocar las anotaciones en las funciones del controlador

--- src/main/java/com/example/katacrud/TodoController.java
+++ src/main/java/com/example/katacrud/TodoController.java
@@ -1,7 +1,7 @@
 package com.example.katacrud;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class TodoController {
@@ -9,19 +9,32 @@ public class TodoController {
     @Autowired
     private TodoService service;
 
+    @GetMapping(value = "api/todos")
     public Iterable<Todo> list() {
         return service.list();
     }
 
-    public Todo save(Todo todo) {
+    @PostMapping(value = "api/todo")
+    public Todo save(@RequestBody Todo todo) {
         return service.save(todo);
     }
 
-    public void delete(Long id) {
+    @PutMapping(value = "api/todo")
+    public Todo update(@RequestBody Todo todo) {
+        if (todo.getId() != null) {
+            return service.save(todo);
+        }
+        throw new RuntimeException("No existe el id para actualizar");
+
+    }
+
+    @DeleteMapping(value = "api/{id}/todo")
+    public void delete(@PathVariable("id") Long id) {
         service.delete(id);
     }
 
-    public Todo get(Long id) {
+    @GetMapping(value = "api/{id}/todo")
+    public Todo get(@PathVariable("id") Long id) {
         return service.get(id);
     }
 }
